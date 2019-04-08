147cea135571c5ea1e6b5b6de90a58f3b9efa2d6
codestk <codestk@gmail.com>
2019-04-08 21:43:01 +0700

    Add project files.

---
+++ BBL.Lib.Rules/DomainCondition.cs
@@ -0,0 +1,47 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Rules;
+using Sitecore.Rules.Conditions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BBL.Lib.Rules
+{
+    public class DomainCondition<T> : StringOperatorCondition<T> where T : RuleContext
+    {
+        public string DomainStringName { get; set; }
+
+        public string DomainStringValue { get; set; }
+        protected override bool Execute(T ruleContext)
+        {
+           
+
+            Assert.ArgumentNotNull(ruleContext, "ruleContext");
+
+            string myDomainStringName = this.DomainStringName ?? string.Empty;
+            string myDomainStringValue = this.DomainStringValue ?? string.Empty;
+
+            if (!string.IsNullOrWhiteSpace(myDomainStringName))
+            {
+                if (HttpContext.Current != null)
+                {
+                    // Populated with QueryString coming into current Page
+                    string incomingDomainStringValue = HttpContext.Current.Request.UrlReferrer.DnsSafeHost ?? string.Empty;
+                    if (string.Equals(incomingDomainStringValue, myDomainStringValue, StringComparison.OrdinalIgnoreCase))
+                    {
+
+                        return true;
+                    }
+                }
+
+
+               
+            }
+
+            return false;
+        }
+    }
+}
---
+++ BBL.Lib.Rules/copy.bat
@@ -0,0 +1,1 @@
+  copy  "C:\Users\hphp\Source\Repos\BBL.Lib.Rules\BBL.Lib.Rules\bin\Debug\BBL.Lib.Rules.dll" C:\Inetpub\wwwroot\sc82u3\Website\bin\ /y
